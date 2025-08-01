f778dce0a7b2ba376fc5162a75190164e7b0889c
Sushmita purbey <sushmitapurbey14@gmail.com>
2025-08-01 21:07:13 +0530

    Add resume to public folder

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -39,7 +39,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/public/projects/Sushmita_s_Resume.pdf"
+                href="/public/Sushmita_s_Resume.pdf"
                 download 
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
