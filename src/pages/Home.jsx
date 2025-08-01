4f9cbf5639524dc90a567f3a9f7f3c7d33a81ab7
Sushmita purbey <sushmitapurbey14@gmail.com>
2025-08-01 14:35:34 +0530

    Fix import path for SkillsSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
-import SkillsSection from "../components/SkillsSection";
+import SkillsSection from "../components/SKillsSection.jsx";
 
 import { ProjectSection } from "../components/ProjectSection";
 import { ContactSection } from "../components/ContactSection";
