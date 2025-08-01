3220e4407baf7d1a4e57d67f8bf99e4862387cb5
Sushmita purbey <sushmitapurbey14@gmail.com>
2025-08-01 14:30:17 +0530

    Add missing SkillsSection component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,8 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
-import { SkillsSection } from "../components/SkillsSection";
+import SkillsSection from "../components/SkillsSection";
+
 import { ProjectSection } from "../components/ProjectSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
